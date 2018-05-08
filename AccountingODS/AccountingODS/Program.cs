880ae99bb2db215a4c5ad1b780edef1bbde752ad
xholodn <456323@mail.muni.cz>
2018-05-08 12:52:35 +0200

    project created

---
+++ AccountingODS/AccountingODS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace AccountingODS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+			MainWindow win = new MainWindow();
+			win.Title = "AccountingODS";
+            win.Show();
+            Application.Run();
+        }
+    }
+}
